1365dee228206c211d99a4a5e6ce8f2d6c7f25cf
orlando <orlandojvelasquezt13@gmail.com>
2026-02-11 20:19:01 -0400

    Configure Backend to use Auth0 environment variables

--- PremezcladosManzanillo/Backend/src/middleware/jwtCheck.ts
+++ PremezcladosManzanillo/Backend/src/middleware/jwtCheck.ts
@@ -1,7 +1,12 @@
 import { auth } from 'express-oauth2-jwt-bearer';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const domain = process.env.AUTH0_DOMAIN || 'dev-3u2pz1qta376worq.us.auth0.com';
+const audience = process.env.AUTH0_AUDIENCE || 'https://premezclados-api.com';
 
 export const jwtCheck = auth({
-  audience: 'https://premezclados-api.com',
-  issuerBaseURL: 'https://dev-bellooswaldo.us.auth0.com/',
+  audience: audience,
+  issuerBaseURL: `https://${domain}/`,
   tokenSigningAlg: 'RS256'
 });
